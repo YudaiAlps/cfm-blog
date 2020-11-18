c00a9d3fcf44440b4f18f1f88fdccecbec8012c4
Yudai YAMAMOTO <kohizeri0804@gmail.com>
2020-11-19 02:12:59 +0900

    easy css

--- resources/views/post/add.blade.php
+++ resources/views/post/add.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 @if(Auth::check())
 @if(count($errors) > 0)
 <p>入力に問題があります。再度入力してください</p>
@@ -7,42 +8,42 @@
     @csrf
       <input type="hidden" name='user_id' value='{{ $user->id }}'>
       @error('title')
-      <tr>
+      <tr class='error'>
       <th>エラー</th>
       <td>{{$message}}</td>
       </tr>
       @enderror
       <tr>
-        <th>title</th>
+        <th>タイトル</th>
         <td><input type="text" name='title' value='{{old("title")}}'></td>
       </tr>
       @error('content')
-        <tr>
+        <tr class='error'>
           <th>エラー</th> 
           <td>{{$message}}</td>
         </tr>
         @enderror
       <tr>
-        <th>content:</th>
+        <th>内容</th>
         <td><textarea name="content" cols="30" rows="10" >{{ old('content') }}</textarea></td>
       </tr>
       <tr>
-        <th>photographs:</th>
+        <th>写真</th>
         <td><input type="file" name='image'></td>
       </tr>
       @error('category')
-        <tr>
+        <tr class='error'>
           <th>エラー</th> 
           <td>{{$message}}</td>
         </tr>
         @enderror
       <tr>
-        <th>category:</th>
+        <th>カテゴリー</th>
         <td><input type="text" name='category' value='{{ old("category") }}'></td>
       </tr>
       <tr>
         <th></th>
-        <td><input type="submit" value='send'></td>
+        <td class='btn'><input type="submit" value='投稿' ></td>
       </tr>
   </table>
 </form>
